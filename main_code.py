48d8552fc058e2585a4868543555f05e1d896c62
Jacob Leisen <jmleisen@icloud.com>
2024-05-19 15:59:52 -0500

    First draft of working bubble sort

--- main_code.py
+++ main_code.py
@@ -1,14 +1,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import time
 
-# Make array for sort
-arr = np.random.randint(1, 100, 20)
+def update_bars(arr, bars):
+        for bar, val in zip(bars, arr):
+            bar.set_height(val)
+        fig.canvas.draw()
+        plt.pause(0.1)
 
-# Make table design
+def bubble_sort(arr):
+    n = len(arr) 
+    bars = ax.bar(range(n), arr, align='center')
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(1, n):
+            if arr[i-1] > arr[i]:
+                arr[i], arr[i-1] = arr[i-1], arr[i]
+                swapped = True
+                update_bars(arr, bars)
+    plt.show()
+
+
+
 fig, ax = plt.subplots()
-ax.bar(range(len(arr)), arr, color="blue")
 ax.axis("off")
-plt.title("Bubble Sort")
-plt.show()
-time.sleep(2)
+arr = np.random.randint(1, 100, 20)
+bubble_sort(arr)
+    
