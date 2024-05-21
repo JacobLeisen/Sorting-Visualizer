4895d0661b7aedbb2d5e25388084b99167e34f41
Jacob Leisen <jmleisen@icloud.com>
2024-05-20 23:04:08 -0500

    Added selection sort function

--- main_code.py
+++ main_code.py
@@ -5,11 +5,9 @@ def update_bars(arr, bars):
         for bar, val in zip(bars, arr):
             bar.set_height(val)
         fig.canvas.draw()
-        plt.pause(0.1)
+        plt.pause(0.01)
 
 def bubble_sort(arr):
-    n = len(arr) 
-    bars = ax.bar(range(n), arr, align='center')
     swapped = True
     while swapped:
         swapped = False
@@ -20,15 +18,33 @@ def bubble_sort(arr):
                 update_bars(arr, bars)
     plt.show()
 
+# Add selection sort
+def selection_sort(arr):
+    for i in range(n - 1):
+        min_index = i
+        for j in range(i, n):
+            if arr[min_index] > arr[j]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+        update_bars(arr, bars)
+    plt.show()
+
 
 # Make table parameters
 fig, ax = plt.subplots()
+arr = np.random.randint(1, 100, 50)
+n = len(arr) 
+bars = ax.bar(range(n), arr, align='center')
 ax.axis("off")
 fig.patch.set_facecolor('lightgrey')
 ax.set_title("Bubble Sort")
-arr = np.random.randint(1, 100, 25)
-bubble_sort(arr)
-    
+selection_sort(arr)
+
+
+
+
+
+
 
 
 # Add insertion sort
