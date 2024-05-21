857be154383897968c2d82f28513b0a928e428fb
Jacob Leisen <jmleisen@icloud.com>
2024-05-21 15:47:45 -0500

    Made speed buttons that work

--- main_code.py
+++ main_code.py
@@ -7,10 +7,19 @@ def update_bars(arr, bars):
         for bar, val in zip(bars, arr):
             bar.set_height(val)
         fig.canvas.draw()
-        plt.pause(0.01)
+        global pause_time
+        plt.pause(pause_time)
 
+# Calls the specific sort on the bars when button is clicked
 def sort_button_click(func):
     func(arr)
+
+# Change the speed of the array
+def change_speed(multiplier):
+    global pause_time 
+    pause_time *= multiplier # Make sure changes global variable
+
+
 
 # Classic bubble sort
 def bubble_sort(arr):
@@ -39,15 +48,16 @@ def selection_sort(arr):
 
 # Make table parameters
 fig, ax = plt.subplots()
-arr = np.random.randint(1, 100, 20)
+arr = np.random.randint(1, 100, 50)
 n = len(arr) 
+pause_time = 0.01
 bars = ax.bar(range(n), arr, align='center')
 ax.axis("off")
 fig.patch.set_facecolor('lightgrey')
 ax.set_title("Sort Visualizer")
 
 # Add Button for Bubble Sort
-bubble_button_ax = plt.axes([0.0, 0.03, 0.15, 0.075])
+bubble_button_ax = plt.axes([0.01, 0.03, 0.15, 0.075])
 bubble_button = Button(bubble_button_ax, "Bubble Sort")
 bubble_button.on_clicked(lambda event: sort_button_click(bubble_sort))
 
@@ -56,17 +66,17 @@ selection_button_ax = plt.axes([0.2, 0.03, 0.15, 0.075])
 selection_button = Button(selection_button_ax, "Selection Sort")
 selection_button.on_clicked(lambda event: sort_button_click(selection_sort))
 
+# Add Speed Buttons
+faster_ax = plt.axes([0.2, 0.9, 0.15, 0.075])
+faster_button = Button(faster_ax, "Faster")
+faster_button.on_clicked(lambda event: change_speed(0.5))
+
+slower_ax = plt.axes([0.01, 0.9, 0.15, 0.075])
+slower_button = Button(slower_ax, "Slower")
+slower_button.on_clicked(lambda event: change_speed(2))
+
 
 plt.show()
-
-
-
-
-
-
-
-
-
 
 
 
@@ -74,7 +84,7 @@ plt.show()
 
 
 
-# Add Speed Buttons
+
 
 # Add Size Buttons
 
